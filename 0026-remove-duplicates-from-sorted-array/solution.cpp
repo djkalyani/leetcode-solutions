59b4f6dbf4f2ab2a66d2c2722c011d3183a2b5aa
Kalyani D J <djkalyani2005@gmail.com>
2025-08-07 01:07:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.7 MB (53.82%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int len = nums.size();
-        int i=0;
-        for(int j=1;j<len;j++){
-            if(nums[j]!=nums[i]){
+        int i = 0;
+        for(int j=1; j<nums.size(); j++){
+            if(nums[i]!=nums[j]){
                 nums[i+1]=nums[j];
                 i++;
             }
+            
         }
         return i+1;
     }
