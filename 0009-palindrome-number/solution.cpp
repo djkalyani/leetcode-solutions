596a63288c85ab5d06fb7c8ad4d1bc77c7758203
Kalyani D J <djkalyani2005@gmail.com>
2025-08-25 16:24:56 +0000

    Sync LeetCode submission Runtime - 2 ms (50.70%), Memory - 8.7 MB (37.69%)

--- 0009-palindrome-number/solution.cpp
+++ 0009-palindrome-number/solution.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        if(x<0)
+        return false;
         long int num = 0;
         int x1;
         int temp = x;
-        while(x>0){
+        while(x!=0){
             x1 = x%10;
+            num = num*10 + x1;
             x=x/10;
-            if(x>0){
-                num = (num+x1)*10; 
-            }
-            else
-            num = num+x1;
-            
         }
+        
         if(temp == num){
             return true;
         }
