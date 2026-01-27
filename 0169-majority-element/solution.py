2ec00f7822855245355b660855b451d3d7319a33
Kalyani D J <djkalyani2005@gmail.com>
2026-01-27 01:14:24 +0000

    Sync LeetCode submission Runtime - 15 ms (18.92%), Memory - 21.1 MB (48.74%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,5 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        '''
         dict1 = {}
         for i in nums:
             dict1[i] = 0
@@ -8,5 +9,27 @@ class Solution:
             if dict1[i]>(len(nums)//2):
                 return i
         
-
-        
+        '''
+        '''
+        my own logic
+        dict1={}
+        majority = floor(len(nums)/2)
+        maj_element = -1
+        for i in range(len(nums)):
+            if nums[i] in dict1:
+                cnt = dict1[nums[i]]
+            else:
+                cnt = nums.count(nums[i])
+            if cnt>majority:
+                dict1[nums[i]]=cnt
+                majority = cnt
+                maj_element = nums[i]
+        return maj_element
+        '''
+        dict1={}
+        for i in nums:
+            dict1[i]=0
+        for i in nums:
+            dict1[i]+=1
+            if dict1[i]>(len(nums)//2):
+                return i
